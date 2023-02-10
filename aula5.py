2c7f49486c0db4d887e9ebfceaa340366b98b7d9
Rafael N <rafaelngoncalves5@outlook.com>
2023-02-10 16:15:21 -0300

    Aula 5

---
+++ aula5.py
@@ -0,0 +1,17 @@
+# Aula 5
+
+# Entrada de dados
+name = input("Digite seu nome: ")
+print(name)
+
+# string.isalpha() -> Verifica se a string tem apenas letras
+# if len(string) >= '': print("String não é nula")
+
+# Fatiando strings
+my_name = 'Rafael N'
+print(my_name[0:8])     # Fatiando a string do índice 0 (no caso R) até o índice 7 (final)
+# ou
+print(my_name[0:])
+# ou
+print(my_name[0:len(my_name)])
+
